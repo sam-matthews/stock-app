17d8d91293149bb0565419ae3bf3542aa92fa147
Sam Matthews <sam,matthews.developer@gmail.com>
2024-12-30 15:47:35 +1300

    Added test function to test sql from yaml file.
    Also run the sql.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import modules.stock_json as stock_json
-import pprint
 import modules.database as database
+import yaml
 
 def main():
     # Read JSON Contents from file.
     mylist=database.demo_load_from_json()
     print(mylist)
-    
+
     # Get Dictionary
     mydict=mylist[0]
     print(mydict.get("create_table"))
@@ -17,5 +16,25 @@ def main():
     with connection:
         connection.execute(mydict.get("create_table"))
 
+def run_yaml():
+
+    print("Starting yaml function")
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+
+    # Access the data
+    print("app_name: " + config["app_name"])  # Output: MyApp
+    print("theme: " + config["settings"]["theme"])  # Output: dark
+    print("sql: "   + config["sql"])  # Output: sql
+
+    sql=config["sql"]
+    print("SQL from variable: " + sql)
+
+    connection = database.connection()
+    with connection:
+        connection.execute(sql)
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    run_yaml()
