3c8295815ca0d7def62c69f0645de055c6bba034
Sam Matthews <sam,matthews.developer@gmail.com>
2024-12-31 16:58:02 +1300

    Moved config.yaml into yaml directory.

--- modules/stock_app.py
+++ modules/stock_app.py
@@ -3,8 +3,10 @@
 import modules.app_yaml as app_yaml
 import modules.database as db
 
+yaml_config="yaml/config.yaml"
+
 def heading():
-    config = app_yaml.open_yaml("config.yaml")
+    config = app_yaml.open_yaml(yaml_config)
 
     print("app_name: " + config["app_name"])
     print("version:  " + str(config["version"]))
@@ -13,12 +15,11 @@ def heading():
 def build_database():
 
     # Open yaml and grab some SQL to run.
-    config = app_yaml.open_yaml("config.yaml")
+    config = app_yaml.open_yaml(yaml_config)
     create_table=config["sql"]["table_sql"]
     drop_table="DROP TABLE " + config["sql"]["table_name"]
 
     # Open DB Connection.
     connection = db.connection()
     with connection:
-        connection.execute(drop_table)
         connection.execute(create_table)
